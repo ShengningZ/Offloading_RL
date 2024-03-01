10cc8f38d40a2e258de9add3e0dc118f589232a5
Shengning Zhang <shengning@dell-hyyltq3.eng.gla.ac.uk>
2024-03-01 17:26:07 +0000

    tracking version2.2 fine grained

---
+++ scripts/start_project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Activating the virtual environment..."
+source yolov5env/bin/activate
+echo "Virtual environment activated."
+# Add any other startup commands here, like opening an IDE or running a Python script
+# e.g., python main.py
